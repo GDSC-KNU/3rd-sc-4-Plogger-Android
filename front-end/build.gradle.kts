125c2ac2956db658c82baceb1d66701731bade24
HJTN <zintnm21@gmail.com>
2024-02-04 13:19:42 +0900

    Create Version Catalog

--- front-end/build.gradle.kts
+++ front-end/build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application") version "8.1.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
 }
+true // Neet to make the Suppress annotation work for the plugins block

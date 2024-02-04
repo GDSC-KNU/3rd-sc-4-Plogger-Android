18e2966c6792f4dc3f2d465304e8b38f21ab2dce
HJTN <zintnm21@gmail.com>
2024-02-04 12:54:09 +0900

    init project

---
+++ front-end/app/src/main/java/com/example/plogger/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.plogger.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.plogger.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

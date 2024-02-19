faaa04882c9add5f12c7973c92b877853eed966b
HJTN <82032792+HJTN@users.noreply.github.com>
2024-02-19 19:52:09 +0900

    Jinsu (#8)
    * Add: Splash, Login, Signup Screen
    
    * add: Tab Menu, Each Fragment View
    
    * Modify: Myprofile View
    
    * Add: Edit Profile, Logout
    
    * Add: Jogging Map View
    
    * Modify: Google Map View
    
    * Add: Community Function, Board Detail View
    
    * Modify: Chatting Function
    
    * Modify: Google Login 구현

--- front-end/app/src/main/java/com/example/plogger/ui/myprofile/MyProfileFragment.kt
+++ front-end/app/src/main/java/com/example/plogger/ui/myprofile/MyProfileFragment.kt
@@ -7,8 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.plogger.ApplicationClass.Companion.sharedPreferencesUtil
+import com.example.plogger.R
 import com.example.plogger.databinding.FragmentMyprofileBinding
 import com.example.plogger.ui.user.LoginActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Scope
 
 class MyProfileFragment : Fragment() {
     lateinit var binding: FragmentMyprofileBinding
@@ -54,9 +59,27 @@ class MyProfileFragment : Fragment() {
             // 로그아웃
             box6.setOnClickListener {
 //                sharedPreferencesUtil.deleteToken()
+
+                getGoogleClient().signOut()
+                startActivity(Intent(activity, LoginActivity::class.java))
+                activity?.finish()
+            }
+            // 회원탈퇴
+            box7.setOnClickListener {
+                getGoogleClient().revokeAccess()
                 startActivity(Intent(activity, LoginActivity::class.java))
                 activity?.finish()
             }
         }
+    }
+
+    private fun getGoogleClient(): GoogleSignInClient {
+        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(Scope("https://www.googleapis.com/auth/pubsub"))
+            .requestServerAuthCode(getString(R.string.google_login_client_id))
+            .requestEmail()
+            .build()
+
+        return GoogleSignIn.getClient(requireActivity(), googleSignInOption)
     }
 }
--- front-end/app/src/main/java/com/example/plogger/ui/user/LoginActivity.kt
+++ front-end/app/src/main/java/com/example/plogger/ui/user/LoginActivity.kt
@@ -3,15 +3,41 @@ package com.example.plogger.ui.user
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.plogger.ApplicationClass.Companion.sharedPreferencesUtil
+import com.example.plogger.R
 import com.example.plogger.databinding.ActivityLoginBinding
 import com.example.plogger.ui.MainActivity
 import com.example.plogger.ui.util.Util.hideKeyboard
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
 //    private val logInViewModel: LogInViewModel by viewModels()
+    private val googleSignInClient: GoogleSignInClient by lazy { getGoogleClient() }
+    private val googleAuthLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+
+        try {
+            val account = task.getResult(ApiException::class.java)
+
+            val userName = account.displayName
+            val userEmail = account.email
+            val serverAuth = account.serverAuthCode
+
+            Log.d("UserInfo", "Name: $userName, UserEmail: $userEmail")
+            startActivity(Intent(this.baseContext, MainActivity::class.java))
+            finish()
+        } catch (e: ApiException) {
+            Log.e("GoogleLoginError", e.stackTraceToString())
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -40,6 +66,25 @@ class LoginActivity : AppCompatActivity() {
             loginSignUp.setOnClickListener {
                 startActivity(Intent(this@LoginActivity, SignupActivity::class.java))
             }
+            googleLoginBtn.setOnClickListener {
+                requestGoogleLogin()
+            }
         }
+    }
+
+    private fun requestGoogleLogin() {
+        googleSignInClient.signOut()
+        val signInIntent = googleSignInClient.signInIntent
+        googleAuthLauncher.launch(signInIntent)
+    }
+
+    private fun getGoogleClient(): GoogleSignInClient {
+        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(Scope("https://www.googleapis.com/auth/pubsub"))
+            .requestServerAuthCode(getString(R.string.google_login_web_client_id))
+            .requestEmail()
+            .build()
+
+        return GoogleSignIn.getClient(this, googleSignInOption)
     }
 }
